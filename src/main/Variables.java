9cc122191b9d8fe161f22e2f92b3093bdab9c191
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-06-12 23:33:15 -0400

    version 1

---
+++ src/main/Variables.java
@@ -0,0 +1,27 @@
+package main;
+// Averiguar que hacen los paquetes
+public class Variables {
+
+	public static void main(String[] args) {
+		/*
+		 * 4 tipos de datos
+		 * 
+		 * enteros 
+		 * decimales
+		 * caracteres
+		 * Booleanos
+		 * */
+		
+		int version;
+		// estamos declarando un espacio en memoria
+		version = 9;
+		version=10;
+		// se ejecutan de manera descendente
+		System.out.println(version);
+		
+		int versionJava=10;
+		System.out.println("Version Java "+versionJava);
+
+	}
+
+}
