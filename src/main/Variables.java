078d180109c8f187343b8c680536cc36b9fb4fda
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-06-13 00:14:30 -0400

    version 3

--- src/main/Variables.java
+++ src/main/Variables.java
@@ -21,7 +21,38 @@ public class Variables {
 		
 		int versionJava=10;
 		System.out.println("Version Java "+versionJava);
-
+		
+		// En java hay 8 tipos de datos
+		//tipos primitivos
+		
+		/*
+		 * Enteros
+		 * 		byte
+		 * 		short
+		 * 		long
+		 * 		int
+		 * Flotantes
+		 * 		float
+		 * 		double
+		 *Caracteres
+		 *		char
+		 *Booleano
+		 *		boolean
+		 * */
+		
+		char letra ='a';
+		char numero=97;
+		System.out.println(letra);
+		System.out.println("Numero "+ numero);
+		//hay mas de 50 Palabras Reservadas
+		
+		// se puede declarar variables en una linea de codigo con valor
+		
+		int bb=1, aa=3;
+		
+		//constante un puerto de una b
+		
+		
 	}
 
 }
